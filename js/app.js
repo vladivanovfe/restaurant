94ce6cb058645555b8517e16631fb87a391338e3
vladivanovgit <vladivanovfe@gmail.com>
2020-01-29 12:34:30 +0200

    add full html and all images

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
-    $('.ba-slider').slick({
+    $('.ba-banner-slider').slick({
         dots: true,
         arrows: false
+    });
+
+    $('.ba-feature-slider').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 500
     });
 })
